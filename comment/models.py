9824e6082894b555de5b13636a8b153f2c94da7a
XiLingf <1178778909@qq.com>
2020-10-12 10:12:57 +0800

    第十一提交，准备上传到云服务器

--- comment/models.py
+++ comment/models.py
@@ -1,7 +1,29 @@
+import threading
+
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import render
+
+class Send_Mail(threading.Thread):
+    def __init__(self, subject, text, email):
+        self.subject = subject
+        self.text = text
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        send_mail(
+            self.subject,
+            '',
+            settings.EMAIL_HOST_USER,
+            [self.email],
+            fail_silently=False,
+            html_message=self.text
+        )
 
 class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -15,6 +37,25 @@ class Comment(models.Model):
     root = models.ForeignKey('self', null=True, related_name="root_comment", on_delete=models.CASCADE)
     parent = models.ForeignKey('self', null=True, related_name="parent_comment", on_delete=models.CASCADE)
     reply_to = models.ForeignKey(User, null=True, related_name="replies", on_delete=models.CASCADE)
+
+    def send_mail(self):
+        # 发送邮件通知
+        if self.parent is None:
+            # 评论我的博客
+            subject = '博客回复通知'
+            email = self.content_object.get_email()
+        else:
+            # 回复博客评论
+            subject = '评论回复通知'            
+            email = self.reply_to.email
+        if email != '':
+            context = {}
+            context['comment_text'] = self.text
+            context['url'] = self.content_object.get_url()
+            text = render(None, 'comment/send_email.html', context).content.decode('utf-8')
+            send_mail = Send_Mail(subject, text, email)
+            send_mail.start()
+
 
     def __str__(self):
         return self.text
